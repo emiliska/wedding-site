2ba7219493c255aa9817c76f0a16d276a76c5d0b
emiliska <33560517+emiliska@users.noreply.github.com>
2018-12-26 23:04:14 -0500

    Version 1 of the site

---
+++ go-app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+	http.HandleFunc("/", handle)
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	content, err:= ioutil.ReadFile("index.html")
+	if err!= nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Fprint(w, string(content))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
